1e10ecbc940df0a93dbf2dc3b7566c05cb9c28c0
bpetermann <benjamin.petermann@gmx.at>
2025-10-06 21:52:48 +0200

    chore: add workspace list files recursion

--- lib/Workspace.ts
+++ lib/Workspace.ts
@@ -2,17 +2,28 @@ import fs from 'fs';
 import path from 'path';
 
 export default class Workspace {
-  constructor(private readonly path: string) {}
+  private readonly IGNORE = ['.git'];
 
-  listFiles(): Array<string> {
-    return fs
-      .readdirSync(this.path, { withFileTypes: true })
-      .filter((item) => !item.isDirectory())
-      .map((item) => item.name);
+  constructor(private readonly rootPath: string) {}
+
+  listFiles(dir: string = this.rootPath): string[] {
+    const entries = fs.readdirSync(dir, { withFileTypes: true });
+
+    return entries
+      .filter((entry) => !this.IGNORE.includes(entry.name))
+      .flatMap((entry) => {
+        const fullPath = path.join(dir, entry.name);
+
+        if (entry.isDirectory()) {
+          return this.listFiles(fullPath);
+        }
+
+        return path.relative(this.rootPath, fullPath);
+      });
   }
 
   readFile(file: string): NonSharedBuffer {
-    return fs.readFileSync(path.join(this.path, file));
+    return fs.readFileSync(path.join(this.rootPath, file));
   }
 
   statFile(path: string): boolean {
