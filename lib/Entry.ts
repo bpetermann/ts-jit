3988ab782188238a62e3006a83884d7891a19e34
bpetermann <benjamin.petermann@gmx.at>
2025-10-08 16:02:09 +0200

    feat: add entry methods

--- lib/Entry.ts
+++ lib/Entry.ts
@@ -1,3 +1,5 @@
+import path from 'path';
+
 export default class Entry {
   private readonly REGULAR_MODE = '100644';
   private readonly EXECUTABLE_MODE = '100755';
@@ -10,5 +12,20 @@ export default class Entry {
 
   get mode(): string {
     return this.executable ? this.EXECUTABLE_MODE : this.REGULAR_MODE;
+  }
+
+  get basename(): string {
+    return path.basename(this.name);
+  }
+
+  descendUntilLast(): string[] {
+    const parts = this.name.split(path.sep);
+    const result: string[] = [];
+
+    for (let i = 0; i < parts.length - 1; i++) {
+      result.push(parts.slice(0, i + 1).join(path.sep));
+    }
+
+    return result;
   }
 }
