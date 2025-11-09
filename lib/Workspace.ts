f52fe19afb2aa352f0dee3b26e2fb5931ce9cf00
bpetermann <benjamin.petermann@gmx.at>
2025-11-09 20:47:42 +0100

    chore: change workspace list files

--- lib/Workspace.ts
+++ lib/Workspace.ts
@@ -1,33 +1,33 @@
 import fs from 'fs';
-import path from 'path';
+import { join, relative } from 'path';
 
 export default class Workspace {
   private readonly IGNORE = ['.git'];
 
   constructor(private readonly rootPath: string) {}
 
-  listFiles(dir: string = this.rootPath): string[] {
-    const entries = fs.readdirSync(dir, { withFileTypes: true });
+  listFiles(path: string = this.rootPath): string[] {
+    if (fs.statSync(path).isDirectory()) {
+      return fs
+        .readdirSync(path, { withFileTypes: true })
+        .filter((entry) => !this.IGNORE.includes(entry.name))
+        .flatMap((entry) => {
+          const fullPath = join(path, entry.name);
+          return entry.isDirectory()
+            ? this.listFiles(fullPath)
+            : [relative(this.rootPath, fullPath)];
+        });
+    }
 
-    return entries
-      .filter((entry) => !this.IGNORE.includes(entry.name))
-      .flatMap((entry) => {
-        const fullPath = path.join(dir, entry.name);
-
-        if (entry.isDirectory()) {
-          return this.listFiles(fullPath);
-        }
-
-        return path.relative(this.rootPath, fullPath);
-      });
+    return [relative(this.rootPath, path)];
   }
 
   readFile(file: string): NonSharedBuffer {
-    return fs.readFileSync(path.join(this.rootPath, file));
+    return fs.readFileSync(join(this.rootPath, file));
   }
 
   statFile(filePath: string): fs.Stats {
-    const fullPath = path.join(this.rootPath, filePath);
+    const fullPath = join(this.rootPath, filePath);
     return fs.statSync(fullPath);
   }
 
