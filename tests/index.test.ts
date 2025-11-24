e174cf9595a78785bf370cc4d3845f68e7ba90dd
bpetermann <benjamin.petermann@gmx.at>
2025-11-24 19:05:30 +0100

    test: add index test suite

---
+++ tests/index.test.ts
@@ -0,0 +1,40 @@
+import { Stats, statSync } from 'fs';
+import Index from 'lib/Index.js';
+import Entry from 'lib/index/Entry.js';
+import { beforeEach, describe, expect, test } from 'vitest';
+import { createTempRepo } from './helper.js';
+
+const getPath = ({ path }: Entry): string => path;
+
+describe('Index.ts', () => {
+  let root: string;
+  let stat: Stats;
+  let oid: string;
+  let index: Index;
+
+  beforeEach(() => {
+    const tmp = createTempRepo();
+    root = tmp.root;
+    stat = statSync(root);
+    oid = Math.random().toString();
+    index = new Index(root);
+  });
+
+  test('adds a single file', () => {
+    index.add('alice.txt', oid, stat);
+
+    const paths = index.eachEntry().map(getPath);
+
+    expect(paths).toEqual(['alice.txt']);
+  });
+
+  test('replace a file with a directory', () => {
+    index.add('alice.txt', oid, stat);
+    index.add('bob.txt', oid, stat);
+    index.add('alice.txt/nested.txt', oid, stat);
+
+    const paths = index.eachEntry().map(getPath);
+
+    expect(paths).toEqual(['alice.txt/nested.txt', 'bob.txt']);
+  });
+});
