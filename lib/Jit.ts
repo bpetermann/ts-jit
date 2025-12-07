a2b1e5e988033c099addb3881d8a0c64b78b0720
bpetermann <benjamin.petermann@gmx.at>
2025-12-07 20:55:32 +0100

    feat: add jit parse command method

--- lib/Jit.ts
+++ lib/Jit.ts
@@ -3,14 +3,25 @@ import { JitCommand } from './types/JitCommand.js';
 
 export default class Jit {
   constructor() {
-    const name = process.argv[2] as JitCommand;
+    const name = process.argv[2];
     const dir = process.argv[3];
     const args = process.argv.slice(3);
 
     try {
-      new Command().dispatch(name, dir, process.cwd(), args);
+      const command = this.parseCommand(name);
+      new Command().dispatch(command, {
+        targetDir: dir,
+        root: process.cwd(),
+        args,
+      });
     } catch (error) {
       console.error(`jit: ${error.message}`);
     }
+  }
+
+  parseCommand(name: string): JitCommand {
+    if (!Object.values(JitCommand).includes(name as JitCommand))
+      throw new Error(`${name} is not a jit command.`);
+    return name as JitCommand;
   }
 }
--- lib/command/Command.ts
+++ lib/command/Command.ts
@@ -1,27 +1,19 @@
-import { JitCommand } from 'lib/types/JitCommand.js';
+import { JitCommand } from '../types/JitCommand.js';
 import Add from './Add.js';
 import Base, { CommandContext } from './Base.js';
 import Commit from './Commit.js';
 import Init from './Init.js';
 
+type JitClass = new (args: CommandContext) => Base;
+
 export default class Command {
-  private readonly commands: Record<
-    JitCommand,
-    new (args: CommandContext) => Base
-  > = {
+  private readonly commands: Record<JitCommand, JitClass> = {
     [JitCommand.Add]: Add,
     [JitCommand.Init]: Init,
     [JitCommand.Commit]: Commit,
   };
 
-  dispatch(name: JitCommand, targetDir: string, root: string, args?: string[]) {
-    if (!this.commands[name]) throw new Error(`${name} is not a jit command.`);
-
-    const ctx: CommandContext = {
-      root,
-      ...(name === JitCommand.Init ? { targetDir } : { args }),
-    };
-
+  dispatch(name: JitCommand, ctx: CommandContext) {
     const commandClass = this.commands[name];
     const command = new commandClass(ctx);
     command.run();
